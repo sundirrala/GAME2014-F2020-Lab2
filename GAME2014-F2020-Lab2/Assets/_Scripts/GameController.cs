b7911163aeecb96c8302305a8b084add1a3194a7
sundirrala <nisara.drahman@gmail.com>
2020-10-02 22:17:04 -0400

    lab 2 commit

---
+++ GAME2014-F2020-Lab2/Assets/_Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (Input.deviceOrientation)
+        {
+            case DeviceOrientation.Portrait:
+                Debug.Log("Portrait");
+                break;
+            case DeviceOrientation.LandscapeLeft:
+                Debug.Log("LandscapeLeft");
+                break;
+            case DeviceOrientation.LandscapeRight:
+                Debug.Log("LandscapeRight");
+                break;
+            case DeviceOrientation.Unknown:
+                Debug.Log("Unkown");
+                break;
+            default:
+                Debug.Log("Some other orientation");
+                break;
+        }
+    }
+}
---
+++ GAME2014-F2020-Lab2/Assets/_Scripts/TestButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TestButton : MonoBehaviour
+{
+    public TMP_Text SceneLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Event Handler for the StartButton_Pressed Event
+    public void OnTestButtonPressed()
+    {
+        Debug.Log("TestButton Pressed");
+        SceneLabel.text = "Test";
+    }
+}
+
+
